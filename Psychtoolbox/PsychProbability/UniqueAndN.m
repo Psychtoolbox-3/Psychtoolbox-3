2e19d4dde9a055a4b37f1be4dd854b380c31d6ab
Diederick C. Niehorster <dcnieho@gmail.com>
2018-06-03 07:08:35 +0200

    UniqueAndN gained byRow option

--- Psychtoolbox/PsychProbability/UniqueAndN.m
+++ Psychtoolbox/PsychProbability/UniqueAndN.m
@@ -1,14 +1,25 @@
-function [b,n] = UniqueAndN(in)
-% [uniques,n] = uniqueen(input)
+function [b,n] = UniqueAndN(in,qByRow)
+% [uniques,n] = uniqueen(input,qByRow)
 %
 % returns the unique elements in input (sorted) and
 % the number of times the item ocurred
+% optionally does so for unique rows (qByRow=true) instead of unique
+% elements
 % 
 % DN    2008
+% DN    2017 Added byRow option
 
-in      = sort(in);  % Necessary for the trick below to work
-
-[b,i,j] = unique(in);
+if nargin<2 || ~qByRow
+    
+    in      = sort(in);  % Necessary for the trick below to work
+    
+    [b,~,j] = unique(in);
+    
+else
+    in      = sortrows(in);
+    
+    [b,~,j] = unique(in,'rows');
+end
 
 d       = diff([0; j(:); max(j)+1]);
 
