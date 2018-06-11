afab48a35b33478273ec838a111d5d33009d1a3d
Diederick C. Niehorster <dcnieho@gmail.com>
2018-06-11 18:02:17 +0200

    improved function following Mario's feedback. Now accepts empty second input, now makes clear what the default is. Better variable naming, consistent function naming in help text and a newline at end of file

--- Psychtoolbox/PsychProbability/UniqueAndN.m
+++ Psychtoolbox/PsychProbability/UniqueAndN.m
@@ -1,24 +1,24 @@
-function [b,n] = UniqueAndN(in,qByRow)
-% [uniques,n] = uniqueen(input,qByRow)
+function [b,n] = UniqueAndN(input,operateByRow)
+% [uniques,n] = UniqueAndN(input,[operateByRow=false])
 %
 % returns the unique elements in input (sorted) and
 % the number of times the item ocurred
-% optionally does so for unique rows (qByRow=true) instead of unique
-% elements
+% optionally does so for unique rows (operateByRow=true)
+% instead of unique elements
 % 
 % DN    2008
 % DN    2017 Added byRow option
 
-if nargin<2 || ~qByRow
+if nargin<2 || isempty(operateByRow) || ~operateByRow
     
-    in      = sort(in);  % Necessary for the trick below to work
+    input   = sort(input);  % Necessary for the trick below to work
     
-    [b,~,j] = unique(in);
+    [b,~,j] = unique(input);
     
 else
-    in      = sortrows(in);
+    input   = sortrows(input);
     
-    [b,~,j] = unique(in,'rows');
+    [b,~,j] = unique(input,'rows');
 end
 
 d       = diff([0; j(:); max(j)+1]);
