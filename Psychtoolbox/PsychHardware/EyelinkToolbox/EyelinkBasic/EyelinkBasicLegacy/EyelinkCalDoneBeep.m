33984c0ef6fc9d2a3dc40f30ab9710ad031d9938
Brian Richardson <brian@sr-research.com>
2023-11-10 14:20:14 -0500

    Eyelink: Restore code and leave deprecation warnings for EyelinkBasic legacy functions. Previously removed in 37eaf45e07df1db36d607cae12e1bc294c49b563 whereas the intended change was instead to add deprecation warnings and not remove/replace the deprecated code.

--- Psychtoolbox/PsychHardware/EyelinkToolbox/EyelinkBasic/EyelinkBasicLegacy/EyelinkCalDoneBeep.m
+++ Psychtoolbox/PsychHardware/EyelinkToolbox/EyelinkBasic/EyelinkBasicLegacy/EyelinkCalDoneBeep.m
@@ -1,4 +1,4 @@
 function err=EyelinkCalDoneBeep(el, error)
-	warning('EyelinkToolbox:LegacyCalDoneBeep',['The function EyelinkCalDoneBeep() is deprecated. Please update your script ', ...
-		'to use the current method for handling camera setup mode callbacks with PsychEyelinkDispatchCallback.m.']);
-	warning('off', 'EyelinkToolbox:LegacyCalDoneBeep');
+warning('EyelinkToolbox:LegacyCalDoneBeep',['The function EyelinkCalDoneBeep() is deprecated. Please update your script ', ...
+    'to use the current method for handling camera setup mode callbacks with PsychEyelinkDispatchCallback.m.']);
+warning('off', 'EyelinkToolbox:LegacyCalDoneBeep');
