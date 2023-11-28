2c62badbe6e4470620623515a6af4d8bf08db839
Brian Richardson <brian@sr-research.com>
2023-11-28 14:58:28 -0500

    Eyelink: Update SR-ResearchDemos to use explicit PsychPortAudio support

--- Psychtoolbox/PsychHardware/EyelinkToolbox/EyelinkDemos/SR-ResearchDemos/GazeContingent/FixWindowFastSamples/EyeLink_FixWindowFastSamples.m
+++ Psychtoolbox/PsychHardware/EyelinkToolbox/EyelinkDemos/SR-ResearchDemos/GazeContingent/FixWindowFastSamples/EyeLink_FixWindowFastSamples.m
@@ -28,9 +28,6 @@ function EyeLink_FixWindowFastSamples(screenNumber)
 %       hdata (contains a list of 8 fields. Only the first 4 values are important:
 %             [uncalibrated target sticker x, uncalibrated target sticker y, target sticker distance in mm, target flags)
 
-% Initialize PsychSound for calibration/validation audio feedback
-InitializePsychSound();
-
 % Some initial parameters:
 fixWinSize = 100; % Width and Height of square fixation window [in pixels]
 fixateTime = 500; % Duration of gaze inside fixation window required before stimulus presentation [ms]
@@ -146,6 +143,23 @@ try
     el.calibrationtargetcolour = repmat(BlackIndex(screenNumber),1,3);
     % set "Camera Setup" instructions text colour so it is different from background colour
     el.msgfontcolour = repmat(BlackIndex(screenNumber),1,3);
+
+    % Initialize PsychSound for calibration/validation audio feedback
+    % EyeLink Toolbox now supports PsychPortAudio integration and interop
+    % with legacy Snd() wrapping. Below we open the default audio device in
+    % output mode as master, create a slave device, and pass the device
+    % handle to el.ppa_pahandle.
+    % el.ppa_handle supports passing either standard mode handle, or as
+    % below one opened as a slave device. When el.ppa_handle is empty, for
+    % legacy support EyelinkUpdateDefaults() will open the default device
+    % and use that with Snd() interop, and close the device handle when
+    % calling Eyelink('Shutdown') at the end of the script.
+    InitializePsychSound();
+    pamaster = PsychPortAudio('Open', [], 8+1);
+    PsychPortAudio('Start', pamaster);
+    pahandle = PsychPortAudio('OpenSlave', pamaster, 1);
+    el.ppa_pahandle = pahandle;
+
     % You must call this function to apply the changes made to the el structure above
     EyelinkUpdateDefaults(el);
     
@@ -410,6 +424,9 @@ catch % If syntax error is detected
     % Print error message and line number in Matlab's Command Window
     psychrethrow(psychlasterror);
 end
+PsychPortAudio('Close', pahandle);
+PsychPortAudio('Close', pamaster);
+
 
 % Function that determines if gaze x y coordinates are within fixation window
     function fix = inFixWindow(mx,my)        
